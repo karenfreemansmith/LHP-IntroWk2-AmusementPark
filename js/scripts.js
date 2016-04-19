2af80c8becbfe86752c2024b3f8cfee150356d59
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-04-19 09:18:10 -0700

    collect info from form and convert height into inches

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
 //alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
 
 //List of rides with height requirements
-//Get height
+
 //if tall enough
 //and if short enough
-//add ride to list
+
 $("#showRides").click(function() {
+  var myFeet = parseInt($("#feet").val());
+  var myInches = parseInt($("#inches").val());
+  myInches += myFeet*12;
+  alert("You are " + myInches + " tall");
   $("#rides").append("<li><h2>The Ride!</h2></li>");
   $("#rides").show();
 });
