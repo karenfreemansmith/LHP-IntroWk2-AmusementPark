22ddb2e6427eeac6656e9df625770b6bb143f056
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-04-19 09:00:40 -0700

    print ride name to webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
 //alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
+
+//List of rides with height requirements
+//Get height
+//if tall enough
+//and if short enough
+//add ride to list
+$("#rides").append("<li><h2>The Ride!</h2></li>");
